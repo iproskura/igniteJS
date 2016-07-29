320593f2cf0a0bc685f8fe3844560404bdd54d31
Ingvar Khors <khors.fb@gmail.com>
2016-07-29 18:08:21 +0300

    readme checklist

--- lesson4_homework.js
+++ lesson4_homework.js
@@ -2,19 +2,48 @@
 //
 // Представьте, что в JavaScript нет метода call, есть только метод apply. Используя apply, напишите метод call.
 
+function myCall() {
+    arguments.splice = [].splice();
+
+
+};
+
+// showFullName.call(user, 'firstName', 'surname');
+// showFullName.apply(user, ['firstName', 'surname']);
+
 
 // TODO ###Задача 2
 //
 // Напишите функцию compose, которая принимает значение в качестве основного параметра и
 // произвольное количество функций в качестве дополнительных параметров.
 //     Функция должна вернуть значение, полученное путем использования основного параметра как аргумента для функций,
-// переданных ей в качестве дополнительных аргументов. Например:
-// ```
-// var doubleTheValue = function(val) { return val * 2; }
-// var addOneToTheValue = function(val) { return val + 1; }
-// compose(5, doubleTheValue) //должно вернуться значение 10
-// compose(5, doubleTheValue, addOneToTheValue) // должно вернуться значение  11
-// ```
+// переданных ей в качестве дополнительных аргументов.
+
+// func2 ( func1 ( arg ));
+//
+// function compose() {
+//
+//     if (arguments.length == 1){
+//         return arguments[0];
+//     }else {
+//
+//
+//
+//     }
+// }
+
+// Например:
+var doubleTheValue = function (val) {
+    return val * 2;
+};
+var addOneToTheValue = function (val) {
+    return val + 1;
+};
+
+// console.log(compose(5));                          //должно вернуться значение 5
+// console.log(compose(5, doubleTheValue));                          //должно вернуться значение 10
+// console.log(compose(5, doubleTheValue, addOneToTheValue));       // должно вернуться значение  11
+
 // Если функции передан только один параметр, она должна вернуть его значение.
 
 
@@ -24,21 +53,25 @@
 //     Он должен возвращать обёртку вокруг f, которая при каждом вызове записывает («логирует») аргументы в log,
 // а затем передает вызов в f.
 //     В этой задаче можно считать, что у функции f ровно один аргумент.
+
+
+function work(a) {
+    /* ... */ // work - произвольная функция, один аргумент
+}
+
+function makeLogging(f, log) {
+
+}
+
+
 //     Работать должно так:
-//     ```
-// function work(a) {
-//   /* ... */ // work - произвольная функция, один аргумент
-// }
-//
-// function makeLogging(f, log) { /* ваш код */ }
-//
-// var log = [];
-// work = makeLogging(work, log);
-//
-// work(1); // 1, добавлено в log
-// work(5); // 5, добавлено в log
-//
-// for (var i = 0; i < log.length; i++) {
-//   alert( 'Лог:' + log[i] ); // "Лог:1", затем "Лог:5"
-// }
-// ```
+var log = [];
+work = makeLogging(work, log);
+
+work(1); // 1, добавлено в log
+work(5); // 5, добавлено в log
+
+for (var i = 0; i < log.length; i++) {
+    console.log('Лог:' + log[i]); // "Лог:1", затем "Лог:5"
+}
+
--- readme.md
+++ readme.md
@@ -3,3 +3,26 @@ ignite js course homework and classwork solutions by Igor Proskura
 
 ### notation
 all *.js output through  ```console.log()``` not ```alert()```
+
+
+####checklist:
+```
+lesson 1:   classwork:  done
+            homework:   done
+
+lesson 2:   classwork:  done
+            homework:   done
+
+lesson 3:   classwork:  done
+            homework:   done
+
+lesson 4:   classwork:  ---
+            homework:   ---
+
+lesson 5:   classwork:  -+-
+            homework:   ---
+
+
+
+
+```
